54cd7edc378cb141f4bfd8b821b99b26723aa665
gaddganepola <ganeypola@gmail.com>
2025-07-23 08:49:17 +0530

    autowire in java

--- src/main/java/com/javabasedconfig/App.java
+++ src/main/java/com/javabasedconfig/App.java
@@ -9,7 +9,18 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        Desktop dt = context.getBean("desktop", Desktop.class);
-        dt.compile();
+//        Desktop dt = context.getBean("desktop", Desktop.class);
+//        dt.compile();
+
+        Alien a = context.getBean(Alien.class);
+        a.code();
+        System.out.println(a.getAge());
+        System.out.println(a.getSalary());
+
+//        Desktop dt1 = context.getBean(Desktop.class);
+//        dt1.compile();
+//
+//        Desktop dt2 = context.getBean(Desktop.class);
+//        dt2.compile();
     }
 }
--- src/main/java/com/javabasedconfig/config/AppConfig.java
+++ src/main/java/com/javabasedconfig/config/AppConfig.java
@@ -1,8 +1,13 @@
 package com.javabasedconfig.config;
 
+import com.javabasedconfig.Alien;
+import com.javabasedconfig.Computer;
 import com.javabasedconfig.Desktop;
+import com.javabasedconfig.Laptop;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
@@ -10,7 +15,22 @@ public class AppConfig {
 //    @Bean(name = "desk")
 //    @Bean(name = {"desktop", "desk", "com"})
     @Bean
+//    @Scope("prototype")
     public Desktop desktop() {
         return new Desktop();
+    }
+
+    @Bean
+    public Alien alien(@Autowired Computer com) {
+        Alien obj = new Alien();
+        obj.setAge(30);
+        obj.setSalary(1000);
+        obj.setCom(com);
+        return obj;
+    }
+
+    @Bean
+    public Laptop lapto() {
+        return new Laptop();
     }
 }
