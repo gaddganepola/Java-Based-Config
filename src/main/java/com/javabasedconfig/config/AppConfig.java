175b8b148b9e8ed2b2fcb89d0e9138497b222282
gaddganepola <ganeypola@gmail.com>
2025-07-23 09:01:30 +0530

    primary and qualifier annotations

--- src/main/java/com/javabasedconfig/config/AppConfig.java
+++ src/main/java/com/javabasedconfig/config/AppConfig.java
@@ -5,8 +5,10 @@ import com.javabasedconfig.Computer;
 import com.javabasedconfig.Desktop;
 import com.javabasedconfig.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
@@ -16,12 +18,44 @@ public class AppConfig {
 //    @Bean(name = {"desktop", "desk", "com"})
     @Bean
 //    @Scope("prototype")
+    @Primary
     public Desktop desktop() {
         return new Desktop();
     }
 
+//    Manual assign
+//    @Bean
+//    public Alien alien() {
+//        Alien obj = new Alien();
+//        obj.setAge(30);
+//        obj.setSalary(1000);
+//        obj.setCom(desktop());
+//        return obj;
+//    }
+
+//    Use Autowire
+//    @Bean
+//    public Alien alien(@Autowired Computer com) {
+//        Alien obj = new Alien();
+//        obj.setAge(30);
+//        obj.setSalary(1000);
+//        obj.setCom(com);
+//        return obj;
+//    }
+
+//    Use Qualifier
+//    @Bean
+//    public Alien alien(@Autowired @Qualifier("laptop") Computer com) {
+//        Alien obj = new Alien();
+//        obj.setAge(30);
+//        obj.setSalary(1000);
+//        obj.setCom(com);
+//        return obj;
+//    }
+
+//    Use Primary Bean
     @Bean
-    public Alien alien(@Autowired Computer com) {
+    public Alien alien(Computer com) {
         Alien obj = new Alien();
         obj.setAge(30);
         obj.setSalary(1000);
@@ -30,7 +64,7 @@ public class AppConfig {
     }
 
     @Bean
-    public Laptop lapto() {
+    public Laptop laptop() {
         return new Laptop();
     }
 }
