7478ab8343073b634c6e5b515713b2bccc2ca4da
gaddganepola <ganeypola@gmail.com>
2025-07-23 11:13:32 +0530

    scope and value annotation implementation

--- src/main/java/com/javabasedconfig/Desktop.java
+++ src/main/java/com/javabasedconfig/Desktop.java
@@ -1,10 +1,12 @@
 package com.javabasedconfig;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Scope("prototype")
 public class Desktop implements Computer{
     @Override
     public void compile() {
